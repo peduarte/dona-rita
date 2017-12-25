c27d663ca9a2243b6405f81bca066c093d5cffee
Pedro Duarte <contact@peduarte.com>
2017-12-25 23:24:23 +0100

    ✨ Add `Story` Page

---
+++ src/pages/story.js
@@ -0,0 +1,110 @@
+import React from 'react';
+
+import { Section } from '../components/Section/Section';
+import { Contact } from '../components/Contact/Contact';
+
+import FullWidthImageRolling from '../images/full-width-image-rolling.jpg';
+import Story from '../images/story.jpg';
+import Family from '../images/family.jpg';
+
+function StoryPage() {
+	return (
+		<div>
+			<Section>
+				<div className="grid">
+					<h1 className="col -block md-push-1 lg-push-2 title">
+						This is our story.<br />
+						<span className="salmon">A family adventure.</span>
+					</h1>
+				</div>
+				<div className="grid">
+					<div className="col fluid md-7 md-push-1 lg-6 lg-push-2">
+						<div
+							className="fake-img -square"
+							style={{ backgroundImage: `url(${Story})` }}
+						/>
+					</div>
+				</div>
+				<div className="grid">
+					<div className="col md-7 lg-5 md-push-1 lg-push-3">
+						<h2>
+							Rita’s saudade<sup className="-yellow">&#9679;</sup>
+						</h2>
+					</div>
+
+					<div className="col md-7 lg-5 md-push-1 lg-push-3">
+						<p className="measure">
+							When Rita left the tropics for Norwich she missed Pão de Queijo so
+							much she started baking it from scratch. For the last 15 years
+							she’s been perfecting her secret family recipe. Now her little
+							cheese bread is so delicious, some people think it’s better than
+							the original. We can’t wait for you to try it.
+						</p>
+					</div>
+
+					<div className="col md-3 lg-2">
+						<p className="small faded footnote -yellow" data-id="&#9679;">
+							Saudade is a feeling of nostalgia and homesickness characteristic
+							of Brazilian temperament. This portuguese word has no translation.
+						</p>
+					</div>
+				</div>
+			</Section>
+
+			<div className="banner">
+				<div
+					className="banner-img -small fake-img -three-x-two"
+					style={{ backgroundImage: `url(${FullWidthImageRolling})` }}
+				/>
+				<div
+					className="banner-img -medium fake-img -sixteen-x-seven"
+					style={{ backgroundImage: `url(${FullWidthImageRolling})` }}
+				/>
+			</div>
+
+			<Section className="-white">
+				<div className="grid">
+					<div className="col md-push-1 lg-push-3">
+						<h2>Family business</h2>
+					</div>
+				</div>
+
+				<div className="grid">
+					<div className="col md-7 lg-5 md-push-1 lg-push-3">
+						<p className="measure">
+							Rita is already renowned for her Pão de Queijo with her family and
+							friends.
+						</p>
+
+						<p className="measure">
+							And none more so than with Clara, her son Pedro’s girlfriend.
+							Allergic to gluten, Pão de Queijo has quickly become a staple in
+							Clara’s diet, and she continues to be in awe of the tasty and
+							fluffy gluten free snacks.
+						</p>
+
+						<p className="measure">
+							A truly family enthused adventure, Rita’s dream is for Londoners
+							to try her delicious Brazilian bites, and with the help of Clara
+							and Pedro, to share them with everyone in town.
+						</p>
+					</div>
+
+					<div className="col fluid md-7 md-push-1 lg-6 lg-push-2">
+						<div
+							className="fake-img -three-x-two"
+							style={{ backgroundImage: `url(${Family})` }}
+						/>
+					</div>
+					<div className="col fluid md-7 md-push-1 lg-6 lg-push-2 small faded">
+						Family photo by <a href="https://munchies.com">Munchies</a>.
+					</div>
+				</div>
+			</Section>
+
+			<Contact />
+		</div>
+	);
+}
+
+export default StoryPage;
