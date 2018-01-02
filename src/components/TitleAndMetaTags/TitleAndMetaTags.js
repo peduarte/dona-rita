eeeae218215abe5c53aeefcfd74ce0b3d766145d
Pedro Duarte <contact@peduarte.com>
2018-01-02 10:16:59 +0100

    🎨 Update `TitleAndMetaTags` with `https` protocol (#82)

--- src/components/TitleAndMetaTags/TitleAndMetaTags.js
+++ src/components/TitleAndMetaTags/TitleAndMetaTags.js
@@ -9,15 +9,15 @@ export function TitleAndMetaTags({ url, pathname, title, description }) {
 			</title>
 
 			<meta property="og:url" content={`${url}/${pathname}`} />
-			<meta property="og:image" content="/social/hero-1.jpg" />
-			<meta property="og:image" content="/social/hero-2.jpg" />
-			<meta property="og:image" content="/social/hero-3.jpg" />
-			<meta property="og:image" content="/social/hero-4.jpg" />
+			<meta property="og:image" content={`${url}/social/hero-1.jpg`} />
+			<meta property="og:image" content={`${url}/social/hero-2.jpg`} />
+			<meta property="og:image" content={`${url}/social/hero-3.jpg`} />
+			<meta property="og:image" content={`${url}/social/hero-4.jpg`} />
 			<meta property="og:title" content={title} />
 			<meta property="og:description" content={description} />
 
 			<meta name="twitter:url" content={`${url}/${pathname}`} />
-			<meta name="twitter:image" content="/social/hero-1.jpg" />
+			<meta name="twitter:image" content={`${url}/social/hero-1.jpg`} />
 			<meta name="twitter:title" content={title} />
 			<meta name="twitter:description" content={description} />
 			<meta name="twitter:site" content="@oidonarita" />
@@ -28,7 +28,7 @@ export function TitleAndMetaTags({ url, pathname, title, description }) {
 }
 
 TitleAndMetaTags.defaultProps = {
-	url: 'http://donarita.co.uk',
+	url: 'https://donarita.co.uk',
 	pathname: '',
 	title: 'Dona Rita',
 	description: 'Brazilian cheese bread. Pão de Queijo.',
